aec8ee59c6c6f621fd9a189b854a9b56d9a9ded2
Kristian <lepka.k.986@gmail.com>
2021-11-03 11:37:36 +0100

    Added   Player Model,   GameSession ViewModel

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            this.CurrentPlayer = new Player
+            {
+                Name = "Scott",
+                Gold = 0
+            };
+        }
+    }
+}
