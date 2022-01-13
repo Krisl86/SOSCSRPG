e4b04f22721a63adf11fefb1f8ad8423af60ed3f
Kristian <lepka.k.986@gmail.com>
2022-01-13 17:52:35 +0100

    Lesson 7.1

---
+++ Engine/Factories/MonsterFactory.cs
@@ -0,0 +1,60 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Factories
+{
+    public static class MonsterFactory
+    {
+        private static List<Monster> _monsters;
+        private static Random _random;
+
+        static MonsterFactory()
+        {
+            // Generating monsters
+            _monsters = new List<Monster>()
+            {
+                new Monster("Snake", "Snake.png", 4, 4, 5, 1),
+                new Monster("Rat", "Rat.png", 5, 5, 5, 1),
+                new Monster("Giant Spider", "GiantSpider.png", 10, 10, 10, 3)
+            };
+
+            _random = new Random();
+
+            // Giving monsters items
+
+            // First monster
+            AddLootItems(_monsters[0], 9001, 25);
+            AddLootItems(_monsters[0], 9002, 75);
+            // Second monster
+            AddLootItems(_monsters[1], 9003, 25);
+            AddLootItems(_monsters[1], 9004, 75);
+            // Third...
+            AddLootItems(_monsters[2], 9005, 25);
+            AddLootItems(_monsters[2], 9006, 75);
+
+        }
+
+        public static Monster GetMonsterById(int monsterId)
+        {
+            try
+            {
+                return _monsters[monsterId];
+            }
+            catch
+            {
+                throw new IndexOutOfRangeException("Monster type ID doesn't exist");
+            }
+        }
+
+        static void AddLootItems(Monster monster, int itemId, int percentageChance)
+        {
+            if (_random.Next(1, 101) <= percentageChance)
+            {
+                monster.Inventory.Add(new ItemQuantity(itemId, 1));
+            }
+        }
+    }
+}
---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,44 @@
+﻿using Engine.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Monster : BaseNotificationClass
+    {
+        private int _hitPoints;
+
+        public string Name { get; private set; }
+        public string ImageName { get; set; }
+        public int MaximumHitPoints { get; private set; }
+        public int HitPoints 
+        {
+            get => _hitPoints;
+            private set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+
+        public int XpReward { get; private set; }
+        public int GoldReward { get; private set; }
+
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+        public Monster(string name, string imageName, int maximumHitPoints, int hitPoints, int xpReward, int goldReward)
+        {
+            Name = name;
+            ImageName = string.Format("/Engine;component/Images/Monsters/{0}", imageName);
+            MaximumHitPoints = maximumHitPoints;
+            HitPoints = hitPoints;
+            XpReward = xpReward;
+            GoldReward = goldReward;
+
+            Inventory = new ObservableCollection<ItemQuantity>();
+        }
+    }
+}
